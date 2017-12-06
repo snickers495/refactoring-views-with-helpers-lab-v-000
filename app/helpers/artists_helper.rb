e973dc6a64eb320c1abc5854d548977ddb984dca
Lisa Lee <snickers495@gmail.com>
2017-12-06 19:25:09 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,7 +1,7 @@
 module ArtistsHelper
   def display_artist(song)
     if song.artist.nil?
-      link_to "Add artist", edit_song_path(song)
+      link_to "Add Artist", edit_song_path(song)
     else
       link_to song.artist_name, artist_path(song.artist)
     end
